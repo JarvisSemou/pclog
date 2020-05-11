d9de7969f7a97c81c4ff6d88b377b8bcb3222680
MouseJW <mouse.jiangwei@qq.com>
2020-05-11 14:37:02 +0800

    增加功能
    1、划分后的日志能放到自定义的目录中
    2、增强对包含空格的源日志名和目标目录名的兼容

--- pclog.bat
+++ pclog.bat
@@ -1,9 +1,11 @@
 @echo off
 chcp 65001 >nul
-if "%~2"=="" (
-	setlocal enableDelayedExpansion
-	goto main
-)
+rem if "%~2"=="" (
+rem	setlocal enableDelayedExpansion
+rem	goto main
+rem )
+setlocal enableDelayedExpansion
+goto main
 
 :main
 	if "%~1" == "" (
@@ -12,29 +14,37 @@ if "%~2"=="" (
         echo !a!
         set b="PID 与程序名的映射列表(pid_proces.txt), 指出 PID 与程序名的对应"
         echo !b!
-        set c="关系。生成的结果放在日志文件所在目录的 log_divided_by_pid 目录内。"
+        set c="关系。生成的结果默认放在日志文件所在目录的 log_divided_by_pid 目录内。"
         echo !c!
+        set d="注意：文件名和目录名不要包含特殊字符，否则脚本会出错，建议只用英文、数字和下划线！"
+        echo !d!
         echo=
-        echo pclog  ^<log file^>
+        echo pclog  ^<log file^>    ^[target folder^]
         echo log file       日志文件
+        echo target folder  目标输出文件夹^(默认为 log_divided_by_pid ^)
         echo=
         echo 例:
-        echo pclog  logname.log
+        echo pclog  logname.log             处理 logname.log 文件
+        echo pclog  logname.log    target   处理 logname.log 文件并将结果放到 target 目录
         goto eof
     )
     set file=%~1
-    if exist ".\log_divided_by_pid" (
-        rd /s /q ".\log_divided_by_pid"
+    set targetFolder=".\log_divided_by_pid"
+    if "%~2" neq "" set targetFolder=%~2
+    if exist "!targetFolder!" (
+        rd /s /q "!targetFolder!"
     )
-    md ".\log_divided_by_pid"
+    md "!targetFolder!"
     echo 日志处理中...
-    for /f "delims=`" %%x in (!file!) do (
-        for /f "tokens=3,6,7,8,9" %%i in ("%%x") do (
-            echo %%x 1>> ".\log_divided_by_pid\%%i.log"
-            if "%%j"=="ActivityManager:" (
-                if "%%k"=="Start" (
-                    if "%%l"=="proc" (
-                        echo %%m >> ".\log_divided_by_pid\pid_proces.txt"
+    for %%t in ("!targetFolder!") do (
+        for /f "usebackq delims=`" %%x in ("!file!") do (
+            for /f "tokens=3,6,7,8,9" %%i in ("%%x") do (
+                echo %%x 1>> "%%~dpnt\%%i.log"
+                if "%%j"=="ActivityManager:" (
+                    if "%%k"=="Start" (
+                        if "%%l"=="proc" (
+                            echo %%m >> "%%~dpnt\pid_proces.txt"
+                        )
                     )
                 )
             )
